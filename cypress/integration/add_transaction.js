f86d1295dcdc5f464fc98550e3a426d84369a019
itdao9 [Fadhillah.Alfatayah] <p130420023@bussan.co.id>
2021-08-08 21:56:58 +0700

    :hammer: Modify datepickerrange in dahsboard

--- cypress/integration/add_transaction.js
+++ cypress/integration/add_transaction.js
@@ -6,11 +6,16 @@ it('add transaction' , function () {
     cy.get('#exampleInputPassword').type("rahasia")
     cy.get('.btn').click()
     cy.get('.input-group > .form-control').type("BR12345{enter}")
-    cy.get('.input-group > .form-control').type("BR23463{enter}")
-    cy.get('.container > .row > :nth-child(1) > .form-group > .form-control').select('5e96cbe292b97300fc903315')
+    cy.get('.input-group > .form-control').type("BR12346{enter}")
+    cy.get('.input-group > .form-control').type("BR12347{enter}")
+    cy.get('.input-group > .form-control').type("BR12348{enter}")
     cy.get(':nth-child(2) > .form-group > .form-control').select('KTP')
-    cy.get('#start_date').type("01-Aug-21");
-    cy.get('#end_date').type("02-Aug-21");
+    cy.get('#start_date').type("29-Aug-21");
+    cy.get('#end_date').type("30-Aug-21");
+    cy.get('.container > .row > :nth-child(1) > .form-group > .form-control').select('5e96cbe292b97300fc903315')
+    cy.get(':nth-child(1) > :nth-child(1) > .form-group > .form-control')
+
+    
     cy.get('#discountValue1').select("2000^1e96cbe292b97300fc321242^Discount_Percent")
     cy.get(':nth-child(4) > .card-body > [type="text"]').type("order aja")
     cy.get('#discountValue2').select("2000^1e96cbe292b97300fc321242^Discount_Percent")
