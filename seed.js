e27bdf2cc3f9487d706dc4d39616c3edb1a27f8e
fadhil.alfatayah <fadhil.alfatayah@gmail.com>
2022-08-05 08:48:52 +0700

    Update seed for pkl

--- seed.js
+++ seed.js
@@ -95,16 +95,31 @@ var data = [
     'documents': [
       {
         _id: mongoose.Types.ObjectId('5e96cbe392b97300fc904315'),
-        name: 'Mirrorless',
+        name: 'Lighting',
         product_id: [
           { _id: mongoose.Types.ObjectId('5e96cbe292b97300fc90bb01') },
         ]
       },
       {
         _id: mongoose.Types.ObjectId('2e96dbe292b97300fc904315'),
-        name: 'Dslr',
+        name: 'Camera',
         product_id: [
           { _id: mongoose.Types.ObjectId('1e96cbe292b97310fc90bb01') },
+        ]
+      },
+
+      {
+        _id: mongoose.Types.ObjectId('2e96dbe292b97300fc904325'),
+        name: 'Lensa',
+        product_id: [
+          { _id: mongoose.Types.ObjectId('5e96cbe292b97300fc90b103') },
+        ]
+      },
+      {
+        _id: mongoose.Types.ObjectId('2e96dbe292b97300f1904325'),
+        name: 'Accessories',
+        product_id: [
+          { _id: mongoose.Types.ObjectId('5e96cbe292b973001c90b193') },
         ]
       },
       
@@ -119,7 +134,7 @@ var data = [
         merkId: mongoose.Types.ObjectId('1e96cbe292b97300fc904315'),
         product_name: 'DSLR Canon EOS 5D Mark III',
         status:"AVALAIBLE",
-        description: 'Canon is proud to present the highly anticipated EOS 5D Mark III. With supercharged EOS performance and stunning full frame, high-resolution image capture, the EOS 5D Mark III is designed to perform. Special optical technologies like the 61-Point High Density Reticular AF and an extended ISO range of 100–25600 expandable to 50 (L), 51200 (H1) and 102400 (H2) make the EOS 5D Mark III ideal for shooting weddings in the studio or out in the field, and great for still photography. Advanced professional-level high definition video capabilities (that includes a host of industry-standard recording protocols and enhanced performance) make it possible to capture beautiful cinematic movies in EOS HD quality. A newly designed 22.3 Megapixel full-frame Canon CMOS sensor, Canon DIGIC 5+ Image Processor, and shooting performance up to 6.0 fps provide exceptional clarity and sharpness, even when capturing rapidly-unfolding scenes.',
+        description: 'Canon is proud to present the highly anticipated EOS 5D Mark III. With supercharged EOS performance and stunning full frame, high-resolution image capture, the EOS 5D Mark III is designed to perform. Special optical technologies like the 61-Point High Density Reticular AF and an extended ISO range of 100–25600 expandable to 50 (L), 51200 (H1) and 102400 (H2) make the EOS 5D Mark III ideal for shooting weddings.',
         image: [
           "https://www.pondoklensa.com/files/photo/web/product/md/58ed9232b95971f79e6329830d9baed63.jpg",
           "https://www.pondoklensa.com/files/photo/web/product/md/cd3978db7406e64f556fe426eb1593341.JPG",
@@ -127,6 +142,66 @@ var data = [
         ],
         price: 325000,
         barcode: 'BR12345',
+      },
+      {
+        _id: mongoose.Types.ObjectId('5e96cbe292b97300fc90bb02'),
+        typeId: mongoose.Types.ObjectId('2e96dbe292b97300fc904315'),
+        merkId: mongoose.Types.ObjectId('1e96cbe292b97300fc904315'),
+        product_name: 'DSLR Canon EOS 700D',
+        status:"AVALAIBLE",
+        description: 'The EOS 700D offers a full and solid basic performance that is clearly one of the best in any entry level DSLR with its high image quality, various functions of Live View AF and movie shooting. The Vari-angle Clear View LCD II with capacitive touch screen capabilities as well as the new 360 degrees rotatable Mode Dial design and Creative Filters will definitely also expand inspiration and creative expression.',
+        image: [
+          "https://www.pondoklensa.com/files/photo/web/product/md/c89cfed0e8c74f8cb4e8545aa7db7a7924.jpg",
+          "https://www.pondoklensa.com/files/photo/web/product/md/7e96f7f24a095d597db5d10ec9685da428.jpg",
+          "https://www.pondoklensa.com/files/photo/web/product/md/0c9814431d79439c99936e77e3b080f728.jpg",
+        ],
+        price: 175000,
+        barcode: 'BR12345',
+      },
+      {
+        _id: mongoose.Types.ObjectId('5e96cbe292b97300fc90b103'),
+        typeId: mongoose.Types.ObjectId('2e96dbe292b97300fc904325'),
+        merkId: mongoose.Types.ObjectId('1e96cbe292b97300fc904315'),
+        product_name: 'Canon EF 14mm f/2.8L II USM',
+        status:"AVALAIBLE",
+        description: 'Ultra-wide angle lenses have always been in demand by architectural, corporate, and other top pro photographers. This new lens features completely redesigned optics including 2 high-precision Aspherical elements and two totally new UD-glass elements. The result is superior image quality: better contrast and sharpness at the outer edges, and a reduction in chromatic aberrations that can sometimes be seen with high-resolution digital SLR.',
+        image: [
+          "https://www.pondoklensa.com/files/photo/web/product/md/41ccbe51b7bdac1a8c8c367a23733def9.jpg",
+          "https://www.pondoklensa.com/files/photo/web/product/md/c9626a6a938189f0d23ef218ef3a1d817.jpg",
+          "https://www.pondoklensa.com/files/photo/web/product/md/41ccbe51b7bdac1a8c8c367a23733def10.jpg",
+        ],
+        price: 175000,
+        barcode: 'BR12345',
+      },
+      {
+        _id: mongoose.Types.ObjectId('5e96cbe292b97300fc90b143'),
+        typeId: mongoose.Types.ObjectId('5e96cbe392b97300fc904315'),
+        merkId: mongoose.Types.ObjectId('1e96cbe292b97300fc904315'),
+        product_name: 'Lighting Studio Best Luck 600W',
+        status:"AVALAIBLE",
+        description: 'The 600W Studio bestluck lamp is a flash that provides a cost-effective lighting solution for photographers. This lamp has almost all the best of QT flashing with good recycling time and flash duration. Various models meet different power output requirements. Suitable for various photography subjects such as marriage, portrait, fashion, and advertising.',
+        image: [
+          "https://www.pondoklensa.com/files/photo/web/product/md/ad0dd1f39349bd186d79a60868c3d8dc22.jpg",
+          "https://www.pondoklensa.com/files/photo/web/product/md/ea45513332d8d7cb05883bc9846e5ce124.jpg",
+          "https://www.pondoklensa.com/files/photo/web/product/md/ad0dd1f39349bd186d79a60868c3d8dc23.jpg",
+        ],
+        price: 125000,
+        barcode: 'BR12355',
+      },
+      {
+        _id: mongoose.Types.ObjectId('5e96cbe292b973001c90b193'),
+        typeId: mongoose.Types.ObjectId('2e96dbe292b97300f1904325'),
+        merkId: mongoose.Types.ObjectId('1e96cbe292b97300fc904315'),
+        product_name: 'Flash Canon Speedlite 430EX III',
+        status:"AVALAIBLE",
+        description: 'The 430 EXIII from Canon is an exceptional enthusiast level Speedlite for your Canon EOS DSLR. Faster and quieter than its predecessor, the 430 EXIII is a discrete lighting option offering plenty of features for such a compact unit.',
+        image: [
+          "https://www.pondoklensa.com/files/photo/web/product/md/37b44ea982e045b9668a1885b8becfd013.jpg",
+          "https://www.pondoklensa.com/files/photo/web/product/md/37b44ea982e045b9668a1885b8becfd014.jpg",
+          "https://www.pondoklensa.com/files/photo/web/product/md/ef949e84be098f948a39046219b1b40319.jpg",
+        ],
+        price: 125000,
+        barcode: 'BR12355',
       },
     ]
   },
